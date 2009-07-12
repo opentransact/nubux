59682726c83a29d22135a3e2d8be1e686b425e4a
Pelle Braendgaard <pelleb@gmail.com>
2009-07-12 21:22:08 +0200

    What a pain, lets try loading smtp_tls before boot.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
+require 'smtp_tls'
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
