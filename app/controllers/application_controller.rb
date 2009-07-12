98c70cc5fda6994b43cabbd988ca6d4f18ec8320
Pelle Braendgaard <pelleb@gmail.com>
2009-07-12 17:29:11 +0200

    Ran OAuth Generator and made it work with clearance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,23 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  alias_method :logged_in?, :signed_in?
+
+  helper_method :logged_in?
+  
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
+  
+  protected 
+
+  alias_method :login_required, :authenticate
+  
+  # To make clearance work with oauth_plugin
+  def current_user
+    @current_user ||= super
+  end
+  
+  def authorized?
+    true
+  end
 end
