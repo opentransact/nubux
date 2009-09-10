ef17a3068db8c359f84e755ec55a937fa9eae809
Pelle Braendgaard <pelleb@gmail.com>
2009-09-10 12:33:58 -0400

    Maddingly Postgresql did not like the double quotes.

--- db/migrate/20090910162408_change_issuer_email.rb
+++ db/migrate/20090910162408_change_issuer_email.rb
@@ -1,9 +1,9 @@
 class ChangeIssuerEmail < ActiveRecord::Migration
   def self.up
-    execute 'update users set email="nubux@stakeventures.com" where email="issuer@nubux.heroku.com"'
+    execute "update users set email='nubux@stakeventures.com' where email='issuer@nubux.heroku.com'"
   end
 
   def self.down
-    execute 'update users set email="issuer@nubux.heroku.com" where email="nubux@stakeventures.com"'
+    execute "update users set email='issuer@nubux.heroku.com' where email='nubux@stakeventures.com'"
   end
 end
